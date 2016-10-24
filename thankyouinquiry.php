635a020b971d0f862f01a3b0b4e3d5cdf52fec48
Tony Tropeano <info@swiftax.com>
2016-10-24 00:22:20 -0400

    updated AIAB

---
+++ thankyouinquiry.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Thank You For Inquiring about Accountant In A Box</title>
+ 
+   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+    <link href="css/style.css" rel="stylesheet">
+   <link rel="stylesheet" href="css/validationEngine.jquery.css" /> <!-- This is for the validation engine -->
+    <!-- Le fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="images/favicon.png">
+
+    <!--open graph-->
+    <meta property="og:title" content="Accountant in a box" />
+    <meta property="og:type" content="video.movie" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="images/opengraph.jpg" />
+</head>
+<body>
+
+<div class="banner-panel thankyou-panel">
+  <div class="container">
+        <a href="index.html#home"><img class="logo" src="images/footer-logo.png" data-lead-id="logo" alt=""></a>
+      </div>
+      <div class="row intro-text">
+        <h1>Thanks for Requesting More Information</h1>
+        <p>You are on your way to increasing the efficiency of your business!</p>
+    </div>
+
+
+            <a href="https://twitter.com/AccountantBox" class="twitter-follow-button share twitter" data-show-count="false" data-size="large">Follow @AccountantBox</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+      
+
+<!-- Calendly link widget begin -->
+<div class="training">
+<div class="row">
+<div class="container">
+
+
+
+<div class="col-xs-12 col-md-12">
+
+     <h3>Schedule A Consultation Session</h3>
+
+     <p>You will receive a registration email within 24 hours. <b>Please schedule your <b class="accent">free</b> software training call with Tony Tropeano, CPA below</b></p>
+<link href="https://calendly.com/assets/external/widget.css" rel="stylesheet">
+<script src="https://calendly.com/assets/external/widget.js" type="text/javascript"></script>
+<a class="btn-signup" href="" onclick="Calendly.showPopupWidget('https://calendly.com/accountantinabox');return false;">Schedule Training Session</a>
+
+        <p class="learn-text"><b>During this FREE training session, you'll learn:</b></p>
+        <ul>
+          <li class="list-group-item">How to set up your Xero account properly and maximize efficiency for bookkeeping and financial reporting.</li>
+          <li class="list-group-item">How to determine which solutions are the most suitable for your business and how to set them up properly.</li>
+          <li class="list-group-item">How to build a good accounting and finance structure for your business.</li>
+        </ul><br>
+        <img src="images/tony.jpg" alt="" >
+        <p>Tony Tropeano
+        <br><b>Founder and CEO</b><br>
+        <i>Accountant In A Box</i></p>
+        </div>
+
+      </div>
+    </div>
+    </div>
+    <!-- End of Training -->
+        <section id="contact">
+        <footer>
+            <div class="container">
+               <div class="footer-first-row">
+                    <div class="row">
+                        <aside class="col-sm-4 col-lg-5">
+                            <figure class="footer-logo"><img src="images/footer-logo.png" alt=""></figure>
+                            <p>Please contact us via email or phone if you have any questions. </p>
+                        </aside>
+
+                        <aside class="col-sm-3 col-lg-3">
+                            <h3>Address</h3>
+                            <ul class="address">
+                                <li>
+                                    <a href="tel:+1-561-596-2724">561-596-2724</a>
+                                </li>
+                                <li>
+                                    <a href="mailto:Info@AccountantInABox.com">Info@AccountantInABox.com</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/maps/place/26%C2%B031'54.5%22N+80%C2%B005'26.1%22W/@26.5317982,-80.0911222,19z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d26.531797!4d-80.090575?hl=en-US" target="_blank">Boynton Beach, FL, USA</a>
+                                </li>
+                            </ul>
+                        </aside>
+                        <aside class="col-sm-3 col-lg-2">
+                            <h3>Connect with us</h3>
+                            <div class="social-links">
+                                <a href="http://www.facebook.com/accountantinabox"><img src="images/footer-fb-icon.png" alt=""></a>
+                                <a href="https://twitter.com/AccountantBox" target="_blank"><img src="images/footer-twitter-icon.png" alt=""></a>
+                                <a href="https://www.linkedin.com/company/accountant-in-a-box"><img src="images/footer-linkedin-icon.png" alt=""></a>
+                            </div>
+                        </aside>
+                    </div>
+                </div>
+                <div class="footer-second-row">
+                    <span class="copy-right">&copy; 2016 Accountant In A Box Inc. All rights reserved</span> 
+                    <div class="footer-links">
+                        <a href="#">Terms of service</a>
+                        <a href="#">Privacy Policy</a>
+                    </div>
+       
+                </div>
+            </div>
+        </footer>
+    </section>
+        <span id="overlay"></span><!--transparent bg color-->
+        
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
+    <script src="http://a.vimeocdn.com/js/froogaloop2.min.js"></script>
+    <!-- FOR JQUERY VALIDATION -->
+    <script src="js/jquery.validationEngine.js"></script>
+    <script src="js/jquery.validationEngine-en.js"></script>
+    <!-- FOR JQUERY VALIDATION -->
+    <script src="js/common.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $("#checkout").validationEngine({promptPosition : "bottomLeft",scroll:false});
+        });
+        
+    </script>
+<!-- Twitter universal website tag code -->
+<script>
+!function(e,n,u,a){e.twq||(a=e.twq=function(){a.exe?a.exe.apply(a,arguments):
+a.queue.push(arguments);},a.version='1',a.queue=[],t=n.createElement(u),
+t.async=!0,t.src='//static.ads-twitter.com/uwt.js',s=n.getElementsByTagName(u)[0],
+s.parentNode.insertBefore(t,s))}(window,document,'script');
+// Insert Twitter Pixel ID and Standard Event data below
+twq('init','nv7t8');
+twq('track','PageView');
+</script>
+<!-- End Twitter universal website tag code -->
+
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-80197283-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- End of GA -->
+  </body>
+</html>
+</body>
+</html>
